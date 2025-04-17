971c9afb6412ee17aadf7eaa4a31f5ed546e86e8
IvanIsaev <79138903153@yandex.ru>
2025-04-17 22:39:22 +0700

    2

---
+++ yandex_disk.py
@@ -0,0 +1,29 @@
+import requests
+
+class YandeDiskApi:
+
+    def __init__(self, token):
+        self.token = token
+        self.base_url = "https://cloud-api.yandex.net/v1/disk/resources"
+        self.headers = {"Authorization": f"OAuth {self.token}"}
+
+    def create_folder(self, path):
+        url = self.base_url
+        params = {"path": path}
+        response = requests.put(url, headers=self.headers, params=params)
+        return response
+
+    def folder_exists(self, path):
+        url = self.base_url
+        params = {"path": path}
+        response = requests.get(url, headers=self.headers, params=params)
+        return response.status_code == 200
+
+    def delete_folder(self, path, permanently=True):
+        url = self.base_url
+        params = {"path": path,
+                  "permanently": str(permanently).lower()}
+        response = requests.delete(url, headers=self.headers, params=params)
+        return response.status_code
+
+
